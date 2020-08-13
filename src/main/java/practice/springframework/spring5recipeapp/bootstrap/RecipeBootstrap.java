232f4129fbf47b1d7479fb038fe2049884ee0039
M10han <vinnu.noolu@gmail.com>
2020-08-13 16:41:30 -0400

    Added more functionalities

--- src/main/java/practice/springframework/spring5recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/practice/springframework/spring5recipeapp/bootstrap/RecipeBootstrap.java
@@ -1,6 +1,7 @@
 package practice.springframework.spring5recipeapp.bootstrap;
 
 
+import lombok.extern.slf4j.Slf4j;
 import practice.springframework.spring5recipeapp.domain.*;
 import practice.springframework.spring5recipeapp.repositories.CategoryRepository;
 import practice.springframework.spring5recipeapp.repositories.RecipeRepository;
@@ -9,11 +10,12 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+@Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -28,8 +30,10 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     }
 
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
         recipeRepository.saveAll(getRecipes());
+        log.debug("Loading bootstrap data");
     }
 
     private List<Recipe> getRecipes() {
--- src/main/java/practice/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/practice/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,13 +1,14 @@
 package practice.springframework.spring5recipeapp.controllers;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import practice.springframework.spring5recipeapp.services.RecipeService;
 
 
-
+@Slf4j
 @Controller
 public class IndexController {
     private final RecipeService recipeService;
@@ -18,6 +19,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model){
+        log.debug("Getting index page");
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
